1aa251f2df956bdd05727126ac7bb0ba45fe666b
YinQin <2774247046@qq.com>
2021-10-19 14:07:12 +0800

    f.

--- src/main/java/com/example/demo2/dao/MSQL_dao.java
+++ src/main/java/com/example/demo2/dao/MSQL_dao.java
@@ -42,49 +42,40 @@ public class MSQL_dao {
     }
     public static void main(String[] args) {
         MSQL_dao dao=new MSQL_dao();
-//        CommodityList list=new CommodityList();
-//        try {
-//            File file = new File("E:\\Android\\daimazhusho\\app\\src\\main\\res\\mipmap-xhdpi\\xz.jpg");
-//            FileInputStream in = new FileInputStream(file);
-//            list.setCommodityName("Nike KD13 cw4115 103 杜兰特 13 贝多芬 白");
-//            list.setCommodityPrice("1999");
-//            byte[] a=new byte[1024];
-//            ByteArrayOutputStream out=new ByteArrayOutputStream();
-//            int len;
-//            byte[] c = new byte[0];
-//            while ((len=in.read(a))!=-1){
-//                out.write(a,0,len);
-//                c=out.toByteArray();
-//            }
-////            File file1 = new File("E://AAA/sb.jpg");
-////            FileOutputStream out1;
-////            try {
-////                out1=new FileOutputStream(file1);
-////                out1.write(c);
-////            }catch (Exception e){
-////                e.printStackTrace();
-////            }
-//            list.setCommodityImg(c);
-//            dao.Insert(list);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        List<CommodityList>list=dao.All();
-        byte []a=new byte[0];
-        for (int i=0;i<list.size();i++){
-            CommodityList commodityList=list.get(i);
-            System.out.println(commodityList.getCommodityName());
-            System.out.println(commodityList.getCommodityPrice());
-            System.out.println(commodityList.getCommodityImg());
-            a=commodityList.getCommodityImg();
-        }
-        ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
-        File file=new File("E://AAA/w.png");
+        CommodityList list=new CommodityList();
         try {
-            FileOutputStream outputStream1=new FileOutputStream(file);
-            outputStream1.write(a);
+            File file = new File("E:\\Android\\daimazhusho\\app\\src\\main\\res\\mipmap-mdpi\\t.jpg");
+            FileInputStream in = new FileInputStream(file);
+            list.setCommodityName("Nike KD13 cw4115 103 杜兰特 13 贝多芬 白");
+            list.setCommodityPrice("1999");
+            byte[] a=new byte[1024];
+            ByteArrayOutputStream out=new ByteArrayOutputStream();
+            int len;
+            byte[] c = new byte[0];
+            while ((len=in.read(a))!=-1){
+                out.write(a,0,len);
+                c=out.toByteArray();
+            }
+            list.setCommodityImg(c);
+            dao.Insert(list);
         } catch (IOException e) {
             e.printStackTrace();
         }
+//        List<CommodityList>list=dao.All();
+//        byte []a=new byte[0];
+//        for (int i=0;i<list.size();i++){
+//            CommodityList commodityList=list.get(i);
+//            System.out.println(commodityList.getCommodityName());
+//            System.out.println(commodityList.getCommodityPrice());
+//            System.out.println(commodityList.getCommodityImg());
+//            a=commodityList.getCommodityImg();
+//        }
+//        File file=new File("E://AAA/w.png");
+//        try {
+//            FileOutputStream outputStream1=new FileOutputStream(file);
+//            outputStream1.write(a);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
